c42d5a3b8b0a3cfcde55d49cdad9d189ada63de5
Joseph <aelphaeis@live.com>
2020-11-25 13:15:05 -0500

    #2 resolves issue #2

---
+++ src/test/java/com/cruat/minesweeper/ProgramTest.java
@@ -0,0 +1,12 @@
+package com.cruat.minesweeper;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class ProgramTest {
+
+	@Test
+	public void testMain() {
+		assertTrue(true);
+	}
+}
