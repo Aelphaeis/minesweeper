f90d4ee717dd7622b5f434f9065d8bec832af99d
Joseph <aelphaeis@live.com>
2020-11-26 01:29:45 -0500

    #3 create default board implementation

---
+++ src/main/java/com/cruat/minesweeper/core/GenericBoard.java
@@ -0,0 +1,65 @@
+package com.cruat.minesweeper.core;
+
+import java.util.Objects;
+
+/**
+ * This class represents a generic board.
+ */
+public class GenericBoard implements Board {
+
+	private final int length;
+	private final int height;
+	private final boolean[][] panels;
+
+	public GenericBoard(boolean[][] panels) {
+		Objects.requireNonNull(panels);
+
+		if (panels.length <= 0) {
+			String err = "Length must be greater than 0";
+			throw new IllegalArgumentException(err);
+		}
+
+		if (panels[0].length <= 0) {
+			String err = "Height must be greater than 0";
+			throw new IllegalArgumentException(err);
+		}
+
+		int expected = panels[0].length;
+		for (int i = 0; i < panels.length; i++) {
+			if (panels[i].length != expected) {
+				String fmt = "Expected column [%s] len to be [%s]. Got: [%s]";
+				String err = String.format(fmt, i, expected, panels[i].length);
+				throw new IllegalArgumentException(err);
+			}
+		}
+
+		length = panels.length;
+		height = expected;
+		this.panels = panels;
+	}
+
+	@Override
+	public boolean isBomb(int x, int y) {
+		return panels[x][y];
+	}
+
+	@Override
+	public int getLength() {
+		return length;
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public String toString() {
+		return new StringBuilder().append("GenericBoard [length=")
+				.append(length)
+				.append(", height=")
+				.append(height)
+				.append("]")
+				.toString();
+	}
+}
