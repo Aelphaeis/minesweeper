7b42f72e526645046a1c7b587116fc700ce23118
Joseph <aelphaeis@live.com>
2020-11-26 01:30:49 -0500

    #3 factory to create board implementations.

---
+++ src/main/java/com/cruat/minesweeper/core/BoardFactory.java
@@ -0,0 +1,27 @@
+package com.cruat.minesweeper.core;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class BoardFactory {
+
+	public Board build(BoardConfig conf) {
+		Random random = Objects.requireNonNull(conf.getNumberGenerator());
+
+		boolean[][] panels = new boolean[conf.getLength()][conf.getHeight()];
+		int panelCount = conf.getLength() * conf.getHeight();
+		for (int i = 0; i < conf.getBombCount(); i++) {
+			boolean retry = false;
+			do {
+				int bombLocation = random.nextInt(panelCount);
+				int x = bombLocation / conf.getLength();
+				int y = bombLocation % conf.getLength();
+
+				retry = panels[x][y];
+				panels[x][y] = true;
+
+			} while (retry);
+		}
+		return new GenericBoard(panels);
+	}
+}
