613c54fbb40e2c6d45d4bf03d0918baaff622961
Joseph <aelphaeis@live.com>
2020-12-03 16:02:13 -0500

    #1 change log message on startup to include date.

--- src/main/java/com/cruat/minesweeper/Program.java
+++ src/main/java/com/cruat/minesweeper/Program.java
@@ -1,6 +1,6 @@
 package com.cruat.minesweeper;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,9 +8,14 @@ import org.apache.logging.log4j.Logger;
 import com.cruat.minesweeper.ui.MainFrame;
 
 public class Program {
+
 	private static final Logger logger = LogManager.getLogger();
+
 	public static void main(String[] args) {
-		logger.info("Starting minesweeper by Aelphaeis at {}", LocalTime.now());
+
+		logger.info("Starting minesweeper by Aelphaeis at {}",
+				() -> LocalDateTime.now());
+
 		MainFrame frame = new MainFrame();
 		frame.createGame();
 		frame.setVisible(true);
