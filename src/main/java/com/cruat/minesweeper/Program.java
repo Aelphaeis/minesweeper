0e2ab3ee8af6a1da5fa708f3aafaf039def948d2
Joseph <aelphaeis@live.com>
2020-12-03 14:26:04 -0500

    #1 open main frame.

--- src/main/java/com/cruat/minesweeper/Program.java
+++ src/main/java/com/cruat/minesweeper/Program.java
@@ -1,5 +1,7 @@
 package com.cruat.minesweeper;
 
+import java.time.LocalTime;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,8 +10,9 @@ import com.cruat.minesweeper.ui.MainFrame;
 public class Program {
 	private static final Logger logger = LogManager.getLogger();
 	public static void main(String[] args) {
+		logger.info("Starting minesweeper by Aelphaeis at {}", LocalTime.now());
 		MainFrame frame = new MainFrame();
+		frame.createGame();
 		frame.setVisible(true);
-		logger.info("Hello World");
 	}
 }
