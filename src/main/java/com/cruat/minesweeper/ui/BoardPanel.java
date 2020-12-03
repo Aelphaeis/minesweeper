12c5c8fa32813dd99bea14697eed44a1f148372a
Joseph <aelphaeis@live.com>
2020-12-03 17:16:30 -0500

    #1 make game playable.

--- src/main/java/com/cruat/minesweeper/ui/BoardPanel.java
+++ src/main/java/com/cruat/minesweeper/ui/BoardPanel.java
@@ -1,30 +1,145 @@
 package com.cruat.minesweeper.ui;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JPanel;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.cruat.minesweeper.core.Board;
 
 public class BoardPanel extends JPanel {
 
+	private static final Logger logger = LogManager.getLogger();
 	private static final long serialVersionUID = 1L;
 	private final Board board;
 
 	public BoardPanel(Board b) {
 		super(new GridLayout(b.getLength(), b.getHeight(), 0, 0));
 		this.board = b;
-		
+
 		for (int y = 0; y < board.getHeight(); y++) {
 			for (int x = 0; x < board.getLength(); x++) {
-				this.add(new GameButton());
+				GameButton btn = new GameButton(x, y);
+				if (board.isBomb(x, y)) {
+					btn.addActionListener(this.new BombButtonActionListener());
+				}
+				else {
+					btn.addActionListener(this.new SafeButtonActionListener());
+				}
+				this.add(btn);
 			}
 		}
 
 		int width = GameButton.MIN_LENGTH * board.getLength();
 		int height = GameButton.MIN_HEIGHT * board.getHeight();
+		this.setSize(new Dimension(width, height));
 		this.setMinimumSize(new Dimension(width, height));
-		this.setSize(getMinimumSize());
+	}
+
+	private void clickButton(int x, int y) {
+		for(Component c : getComponents()) {
+			if(!(c instanceof GameButton)) {
+				continue;
+			}
+			GameButton btn = (GameButton) c;
+			int bx = btn.getBoardX();
+			int by = btn.getBoardY();
+			
+			if(bx == x && by == y && btn.isEnabled()) {
+				btn.doClick();
+			}
+		}
+	}
+
+	private class SafeButtonActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object source = e.getSource();
+			if (!(source instanceof GameButton)) {
+				return;
+			}
+			GameButton btn = ((GameButton) source);
+			btn.setEnabled(false);
+
+			int x = btn.getBoardX();
+			int y = btn.getBoardY();
+			logger.info("button ({}, {}) clicked", x, y);
+
+
+			List<int[]> investigated = new ArrayList<>();
+			int b = 0;
+
+			logger.trace("calculating value for ({}, {})", x, y);
+			for (int xo = -1; xo < 2; xo++) {
+				for (int yo = -1; yo < 2; yo++) {
+					if (yo == 0 && xo == 0) {
+						// this is currently clicked button
+						continue;
+					}
+
+					int cx = x + xo;
+					int cy = y + yo;
+					
+					investigated.add(new int[] { cx, cy });
+
+					if (withinXBound(cx) && withinYBound(cy)) {
+						if (BoardPanel.this.board.isBomb(cx, cy)) {
+							logger.trace("({}, {}) is a bomb", cx, cy);
+							b++;
+						}
+						else {
+							logger.trace("({}, {}) is not a bomb", cx, cy);
+						}
+					}
+					else {
+						logger.trace("({}, {}) is an invalid location", cx, cy);
+					}
+				}
+			}
+
+			logger.trace("({}, {}) is {}", x, y, investigated.size());
+			if (b == 0) {
+				for (int[] cord : investigated) {
+					BoardPanel.this.clickButton(cord[0], cord[1]);
+				}
+			}
+			else {
+				btn.setText(String.valueOf(b));
+			}
+
+		}
+
+		private boolean withinXBound(int n) {
+			int max = BoardPanel.this.board.getLength();
+			return n >= 0 && n < max;
+		}
+
+		private boolean withinYBound(int n) {
+			int max = BoardPanel.this.board.getHeight();
+			return n >= 0 && n < max;
+		}
+	}
+
+	private class BombButtonActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object source = e.getSource();
+			if (!(source instanceof GameButton)) {
+				return;
+			}
+			((GameButton) source).setEnabled(false);
+			((GameButton) source).setText("*");
+
+		}
 	}
 }
--- src/main/java/com/cruat/minesweeper/ui/GameButton.java
+++ src/main/java/com/cruat/minesweeper/ui/GameButton.java
@@ -1,29 +1,29 @@
 package com.cruat.minesweeper.ui;
 
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 
 public class GameButton extends JButton {
 
 	private static final long serialVersionUID = 1L;
-	
-	public static final int MIN_LENGTH = 30;
-	public static final int MIN_HEIGHT = 30;
+	public static final int MIN_LENGTH = 50;
+	public static final int MIN_HEIGHT = 50;
+	private final int boardX;
+	private final int boardY;
 
-	public GameButton() {
-		addActionListener(this.new DisableButtonListener());
+	public GameButton(int boardX, int boardY) {
+		this.boardX = boardX;
+		this.boardY = boardY;
+
 		setMinimumSize(new Dimension(MIN_LENGTH, MIN_HEIGHT));
 	}
-	
-	private class DisableButtonListener implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			GameButton.this.setEnabled(false);
-			GameButton.this.removeActionListener(this);
-		}
+	public int getBoardX() {
+		return boardX;
+	}
+
+	public int getBoardY() {
+		return boardY;
 	}
 }
