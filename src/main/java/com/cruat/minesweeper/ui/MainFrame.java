88d35dfb1f6a099f427a5752103a3516bd51301d
Joseph <aelphaeis@live.com>
2020-12-03 14:24:44 -0500

    #1 create main game window.

--- src/main/java/com/cruat/minesweeper/ui/MainFrame.java
+++ src/main/java/com/cruat/minesweeper/ui/MainFrame.java
@@ -1,15 +1,55 @@
 package com.cruat.minesweeper.ui;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.cruat.minesweeper.core.Board;
+import com.cruat.minesweeper.core.BoardConfigPresets;
+import com.cruat.minesweeper.core.BoardConfigSpec;
+import com.cruat.minesweeper.core.BoardFactory;
 
 public class MainFrame extends JFrame {
+	
+	private static final int BTN_LENGTH = 30;
+	private static final int BTN_HEIGHT = 30;
+
 
 	private static final long serialVersionUID = 1L;
+	private final BoardFactory boardFactory;
 
 	public MainFrame() {
 		super("Minesweeper by Aelphaeis");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500, 500);
+		
+
+		this.boardFactory = new BoardFactory();
 	}
 
+	public void createGame() {
+		createGame(BoardConfigPresets.EXPERT);
+	}
+
+	public void createGame(BoardConfigSpec specification) {
+		Board board = boardFactory.build(specification);
+
+		int rows = board.getHeight();
+		int cols = board.getLength();
+
+		GridLayout layout = new GridLayout(rows, cols);
+		JPanel panel = new JPanel(layout);
+
+		for (int x = 0; x < board.getLength(); x++) {
+			for (int y = 0; y < board.getHeight(); y++) {
+				panel.add(new GameButton());
+			}
+		}
+		
+		
+		setSize(cols * BTN_LENGTH, rows * BTN_HEIGHT);
+		setMinimumSize(getSize());
+		this.add(panel);
+
+	}
 }
